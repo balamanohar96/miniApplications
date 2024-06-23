c8b9eed64da52f5bfc426e0f37dcacd63a653473
bala manohar <balamanohar.yeminedi@gmail.com>
2024-06-23 16:17:53 +0530

    updated copy inputs functionality

--- src/components/Fourth.js
+++ src/components/Fourth.js
@@ -3,33 +3,40 @@ import React, { useState } from "react";
 function Fourth() {
   let [namesobj, setNamesobj] = useState({ firstname: "", secondname: "" });
   let [fullname, setFullname] = useState("");
+  let [isShow, setIsShow] = useState(false);
 
   let gohandler = () => {
     setFullname(
       `your full name is ${namesobj.firstname} ${namesobj.secondname}`
     );
+    setIsShow(true);
   };
   return (
     <div className="first para">
       <p>
-        This is fourth page, In this page, when "submit" button is clicked, fullName is printed below
+        This is fourth page, In this page, when "submit" button is clicked,
+        fullName is printed below
       </p>
       <input
         type="text"
         placeholder="first name"
-        onChange={(e) =>
-          setNamesobj({ ...namesobj, firstname: e.target.value })
-        }
+        onChange={(e) => {
+          setNamesobj({ ...namesobj, firstname: e.target.value });
+          setIsShow(false);
+        }}
       ></input>
       <input
         type="text"
         placeholder="second name"
-        onChange={(e) =>
-          setNamesobj({ ...namesobj, secondname: e.target.value })
-        }
+        onChange={(e) => {
+          setNamesobj({ ...namesobj, secondname: e.target.value });
+          setIsShow(false);
+        }}
       ></input>
       <button onClick={gohandler}>submit</button>
-      {namesobj.firstname && namesobj.secondname && <h4>{fullname}</h4>}
+      {namesobj.firstname && namesobj.secondname && isShow && (
+        <h4>{fullname}</h4>
+      )}
     </div>
   );
 }
